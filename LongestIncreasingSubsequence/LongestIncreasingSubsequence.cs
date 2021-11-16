264de4aa6e484ab2cc5f2139e2dca0b3f5afd5d8
Saad Baig <67672924+saadzibaig@users.noreply.github.com>
2021-11-16 19:43:22 +1100

    Add project files.

---
+++ LongestIncreasingSubsequence/LongestIncreasingSubsequence.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace LongestIncreasingSubsequence
+{
+    public static class LongestIncreasingSubsequence
+    {
+        public static string Get (string input)
+        {
+            //Return empty if string is empty or null
+            if (string.IsNullOrWhiteSpace(input))
+                return "";
+
+            var stringArray = input.Split(" ");
+
+            if (stringArray.Length == 1)
+                return stringArray[0];
+
+            //Checking if all values are numbers
+            if (!AreAllIntegers(stringArray))
+                return "";
+
+            //Convert string array into long array
+            var numberArray = ConvertToLongArray(stringArray);
+
+            int longestLength, resultIndex, tempIndex;
+
+            longestLength = resultIndex = tempIndex = 0;
+
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                if (i > 0 && numberArray[i - 1] >= numberArray[i])
+                {
+                    tempIndex = i;
+                }
+
+                if (longestLength < i - tempIndex + 1)
+                {
+                    longestLength = i - tempIndex + 1;
+                    resultIndex = i;
+                }
+            }
+
+            var startIndex = resultIndex - longestLength + 1;
+
+            var endIndex = resultIndex + 1;
+
+            var tempOutput = numberArray.Skip(startIndex).Take(endIndex - startIndex);
+
+            var output = string.Join(" ", tempOutput);
+            
+            return output;
+        }
+
+        private static Boolean AreAllIntegers (string[] input)
+        {
+            foreach (var value in input)
+            {
+                bool isInteger = int.TryParse(value, out int i);
+
+                if (!isInteger)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static long[] ConvertToLongArray(string[] input)
+        {
+            return Array.ConvertAll(input, s => long.Parse(s));
+        }
+
+    }
+}
